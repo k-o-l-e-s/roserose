76733e754b0d2a537efc161438df2ad2d78ab30f
Koles <koles.near@protonmail.com>
2023-08-09 20:35:36 +0200

    enum

---
+++ src/Enums/ItemEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+enum ItemEnum: string
+{
+
+    case AGED_BRIE = 'Aged Brie';
+    case BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert';
+    case SULFURAS = 'Sulfuras, Hand of Ragnaros';
+
+}
